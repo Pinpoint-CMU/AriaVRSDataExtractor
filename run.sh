7c618a15f4089a49b7bc702499370069f55fbf58
Vivek Roy <thezealousfool@gmail.com>
2022-03-22 12:04:38 -0400

    Fix path errors in script

--- run.sh
+++ run.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  VRS_EXEC="./vivek_vrs"
+  VRS_EXEC="$SCRIPT_DIR/vivek_vrs"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  VRS_EXEC="./vivek_vrs_mac"
+  VRS_EXEC="$SCRIPT_DIR/vivek_vrs_mac"
 else
   echo "Unsupported OS"
   exit 1
 fi
 
-for var in "$@"
+for val in "$@"
 do
-  dir=$(dirname -- "$var")
-  ls "$dir/*.vrs" | xargs -I {} "$VRS_EXEC" "{}"
-  ls "$dir/*.db" | xargs -I {} "./db.sh" "{}"
-  python3 "./imu_alignment.py" "$dir" > "$dir/alignment.csv"
+  dir=$(cd -- "$SCRIPT_DIR/$val" && pwd)
+  echo "Running on $dir"
+  find "$dir" -type f -name "*.vrs" | xargs -I {} "$VRS_EXEC" "{}"
+  find "$dir" -type f -name "*.db" | xargs -I {} "$SCRIPT_DIR/db.sh" "{}"
+  python3 "$SCRIPT_DIR/imu_alignment.py" "$dir" > "$dir/alignment.csv"
 done
