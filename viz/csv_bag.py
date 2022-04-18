f1df251e406d629a715f6b5bad73167a583602b6
Vivek Roy <thezealousfool@gmail.com>
2022-04-18 19:17:53 -0400

    Visualizer for RViz

--- viz/csv_bag.py
+++ viz/csv_bag.py
@@ -1,24 +1,173 @@
 import csv
+import math
 import sys
+import time
 from pathlib import Path
 
+import numpy as np
 from rosbags.rosbag1 import Writer
 from rosbags.serde import cdr_to_ros1, serialize_cdr
+from rosbags.typesys.types import builtin_interfaces__msg__Time as Time
 from rosbags.typesys.types import geometry_msgs__msg__Point as Point
+from rosbags.typesys.types import geometry_msgs__msg__Pose as Pose
+from rosbags.typesys.types import \
+    geometry_msgs__msg__PoseWithCovariance as PoseWithCovariance
+from rosbags.typesys.types import geometry_msgs__msg__Quaternion as Quaternion
+from rosbags.typesys.types import geometry_msgs__msg__Transform as Transform
+from rosbags.typesys.types import \
+    geometry_msgs__msg__TransformStamped as TransformStamped
+from rosbags.typesys.types import geometry_msgs__msg__Twist as Twist
+from rosbags.typesys.types import \
+    geometry_msgs__msg__TwistWithCovariance as TwistWithCovariance
+from rosbags.typesys.types import geometry_msgs__msg__Vector3 as Vector3
+from rosbags.typesys.types import nav_msgs__msg__Odometry as Odometry
+from rosbags.typesys.types import sensor_msgs__msg__Imu as Imu
+from rosbags.typesys.types import sensor_msgs__msg__MagneticField as Mag
+from rosbags.typesys.types import std_msgs__msg__Header as Header
+from rosbags.typesys.types import tf2_msgs__msg__TFMessage as TFMessage
 
 
 def process(input: Path, output: Path):
     assert input.suffix == ".csv"
     with Writer(output) as writer:
-        topic = "/position"
-        msgtype = Point.__msgtype__
-        connection = writer.add_connection(topic, msgtype)
+        traj_topic = "/traj"
+        traj_msgtype = Odometry.__msgtype__
+        traj_connection = writer.add_connection(traj_topic, traj_msgtype)
+        imu_topic = "/imu"
+        imu_msgtype = Imu.__msgtype__
+        imu_connection = writer.add_connection(imu_topic, imu_msgtype)
+        mag_topic = "/mag"
+        mag_msgtype = Mag.__msgtype__
+        mag_connection = writer.add_connection(mag_topic, mag_msgtype)
+        start_time = time.time_ns()
+        start_time_s = int(start_time / 1e9)
+        start_time_ns = int(start_time % int(1e9))
+        csv_start_time = None
 
-        for i in range(1000):
-            message = Point(x=1 + i, y=2 + i, z=3)
-            writer.write(
-                connection, i + 1, cdr_to_ros1(serialize_cdr(message, msgtype), msgtype)
-            )
+        writer.write(
+            writer.add_connection("/tf", TFMessage.__msgtype__),
+            start_time,
+            cdr_to_ros1(
+                serialize_cdr(
+                    TFMessage(
+                        transforms=[
+                            TransformStamped(
+                                header=Header(
+                                    frame_id="map",
+                                    stamp=Time(sec=start_time_s, nanosec=start_time_ns),
+                                ),
+                                child_frame_id="traj",
+                                transform=Transform(
+                                    translation=Vector3(x=0, y=0, z=0),
+                                    rotation=Quaternion(x=0, y=0, z=0, w=1),
+                                ),
+                            )
+                        ]
+                    ),
+                    TFMessage.__msgtype__,
+                ),
+                TFMessage.__msgtype__,
+            ),
+        )
+
+        with open(input, "r") as csvfile:
+            csv_reader = csv.DictReader(csvfile)
+            for row in csv_reader:
+                # timestamp,iphoneAccX,iphoneAccY,iphoneAccZ,iphoneGyroX,iphoneGyroY,iphoneGyroZ,iphoneMagX,iphoneMagY,iphoneMagZ,
+                # orientW,orientX,orientY,orientZ,
+                # processedPosX,processedPosY,processedPosZ
+                if csv_start_time is None:
+                    csv_start_time = float(row["timestamp"])
+                dt = float(row["timestamp"]) - csv_start_time
+                odometry = Odometry(
+                    header=Header(
+                        frame_id="traj",
+                        stamp=Time(
+                            sec=start_time_s + int(math.floor(dt / 1e9)),
+                            nanosec=int(math.ceil(dt % 1e9)),
+                        ),
+                    ),
+                    child_frame_id="traj",
+                    pose=PoseWithCovariance(
+                        pose=Pose(
+                            position=Point(
+                                x=float(row["processedPosX"]),
+                                y=float(row["processedPosY"]),
+                                z=float(row["processedPosZ"]),
+                            ),
+                            orientation=Quaternion(
+                                x=float(row["orientX"]),
+                                y=float(row["orientY"]),
+                                z=float(row["orientZ"]),
+                                w=float(row["orientW"]),
+                            ),
+                        ),
+                        covariance=np.zeros((36,), dtype=np.float64),
+                    ),
+                    twist=TwistWithCovariance(
+                        twist=Twist(linear=Vector3(0, 0, 0), angular=Vector3(0, 0, 0)),
+                        covariance=np.zeros((36,), dtype=np.float64),
+                    ),
+                )
+                writer.write(
+                    traj_connection,
+                    start_time + int(math.floor(dt)),
+                    cdr_to_ros1(serialize_cdr(odometry, traj_msgtype), traj_msgtype),
+                )
+                # timestamp,iphoneAccX,iphoneAccY,iphoneAccZ,iphoneGyroX,iphoneGyroY,iphoneGyroZ,iphoneMagX,iphoneMagY,iphoneMagZ,
+                imu = Imu(
+                    header=Header(
+                        frame_id="imu",
+                        stamp=Time(
+                            sec=start_time_s + int(math.floor(dt / 1e9)),
+                            nanosec=int(math.ceil(dt % 1e9)),
+                        ),
+                    ),
+                    orientation=Quaternion(
+                        x=float(row["orientX"]),
+                        y=float(row["orientY"]),
+                        z=float(row["orientZ"]),
+                        w=float(row["orientW"]),
+                    ),
+                    orientation_covariance=np.zeros((9,), dtype=np.float64),
+                    angular_velocity=Vector3(
+                        x=float(row["iphoneGyroX"]),
+                        y=float(row["iphoneGyroY"]),
+                        z=float(row["iphoneGyroZ"]),
+                    ),
+                    angular_velocity_covariance=np.zeros((9,), dtype=np.float64),
+                    linear_acceleration=Vector3(
+                        x=float(row["iphoneAccX"]),
+                        y=float(row["iphoneAccY"]),
+                        z=float(row["iphoneAccZ"]),
+                    ),
+                    linear_acceleration_covariance=np.zeros((9,), dtype=np.float64),
+                )
+                writer.write(
+                    imu_connection,
+                    start_time + int(math.floor(dt)),
+                    cdr_to_ros1(serialize_cdr(imu, imu_msgtype), imu_msgtype),
+                )
+                mag = Mag(
+                    header=Header(
+                        frame_id="mag",
+                        stamp=Time(
+                            sec=start_time_s + int(math.floor(dt / 1e9)),
+                            nanosec=int(math.ceil(dt % 1e9)),
+                        ),
+                    ),
+                    magnetic_field=Vector3(
+                        x=float(row["iphoneMagX"]),
+                        y=float(row["iphoneMagY"]),
+                        z=float(row["iphoneMagZ"]),
+                    ),
+                    magnetic_field_covariance=np.zeros((9,), dtype=np.float64),
+                )
+                writer.write(
+                    mag_connection,
+                    start_time + int(math.floor(dt)),
+                    cdr_to_ros1(serialize_cdr(mag, mag_msgtype), mag_msgtype),
+                )
 
 
 if __name__ == "__main__":
